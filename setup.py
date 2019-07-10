ed8e303180eb9e00213e0551c425e5319e893280
SMRuggiero <stevemruggiero@gmail.com>
2019-07-10 10:17:25 -0500

    Updated file path of sphinx documentation

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding = "utf8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="sbmltoodepy_SMRuggiero",
-    version="0.0.1",
+    version="0.0.2",
     author="SMR",
     author_email="SteveMRuggiero@gmail.com",
     description="A small example package",
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
+    package_data = {
+        'sbmltoodepy' : ['sbml_files/*.xml']
+    },
     install_requires=[
             'python-libsbml',
             'numpy'
