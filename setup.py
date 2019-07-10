f1185feff20b7fdde10988910ea825205fd0d6a8
SMRuggiero <stevemruggiero@gmail.com>
2019-07-10 08:41:05 -0500

    Updated README and setup files

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
 )
