82a284a4dd65ca05ea10a884a5112f5b06aa76d3
SMRuggiero <stevemruggiero@gmail.com>
2019-07-31 16:58:12 -0500

    Updated authors in LICENSE and setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding = "utf8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sbmltoodepy_SMRuggiero",
-    version="0.0.3",
-    author="SMR",
+    name="sbmltoodepy",
+    version="1.0.1",
+    author="Steve M Ruggiero, Ashlee N Ford Versypt",
     author_email="SteveMRuggiero@gmail.com",
-    description="A small example package",
+    description="A package that creates a python implementation of an SBML model.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/SMRuggiero/sbmltoodepy",
     packages=setuptools.find_packages(),
     package_data = {
         'sbmltoodepy' : ['sbml_files/*.xml']
